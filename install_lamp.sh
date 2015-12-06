55c9a9297703139c380f74254fd7dec051778e00
EricShih <eric12345566@gmail.com>
2015-12-06 12:14:11 +0800

    first commit

---
+++ install_lamp.sh
@@ -0,0 +1,24 @@
+echo "This is a install script of lamp web server for ubuntu"
+echo "Don't leave your computer, you need to enter something when it installing"
+
+#install apache2
+
+sudo apt-get install apache2
+
+#install mysql-server
+
+sudo apt-get install mysql-server
+
+#install php
+
+sudo apt-get install php5
+
+#install php5-mysql
+
+sudo apt-get install php5-mysql
+
+#install phpmyadmin
+
+sudo apt-get install phpmyadmin
+
+echo "All install is complete" 
