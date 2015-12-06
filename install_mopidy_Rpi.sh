d096845665b5d23f85a9303e6742e218189c567a
EricShih <eric12345566@gmail.com>
2015-12-06 22:36:51 +0800

    Complete add mopidy installer

--- install_mopidy_Rpi.sh
+++ install_mopidy_Rpi.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#author: Easy Installer 
+
 #This script just for Raspberry pi. If you're on PC please run another script(Go to  Easy installer Github page to download:https://github.com/eric12345566/easy_installer)
 
 echo "Easy Installer script for mopidy_raspberry install"
@@ -22,13 +24,16 @@ if [ "${yn}" == "Y" ] || [ "${yn}" == "y" ]; then
 	sudo apt-get update
 	sudo apt-get install mopidy
 
-	sudo apt-get install python-pipe
+	sudo apt-get install python-pip
 
-	pip install Mopidy-MusicBox-Webclient
+	sudo pip install Mopidy-MusicBox-Webclient
 
-	echo "Well down, Now  "
-		exit 0
-		fi
+	echo "Now you can run 'mopidy' on your terminal. and than please open 'http://127.0.0.1:6680/' in browser"
+	
+	read -n1 -r -p "and press any key to continue..." key
+	
+	echo "Thanks for using Easy Installer Script!"
+	exit 0
 		if [ "${yn}" == "N" ] || [ "${yn}" == "n" ]; then
 			echo "This script just for raspberry pi!! We have another for PC! Check on Easy Installer on Github(https://github.com/eric12345566/easy_installer) "
 				exit 0
